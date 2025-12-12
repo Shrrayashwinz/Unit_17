9a636b493276110a1dfa2d3462cca5868c61e9fd
Shrrayashwinz <sshrrayash@gmail.com>
2025-12-12 18:46:37 -0500

    Got started

---
+++ lab17_ssrinivasan3-1.py
@@ -0,0 +1,9 @@
+"""
+Program Name: lab17_ssrinivasan3-1.py
+
+Author: Shrrayash Srinivasan
+
+Purpose: Refactor the HN submission script to safely handle missing data fields and prevent KeyError crashes.
+
+Date: December 11, 2025 
+"""
