0b2bc101a96a5ef8ee9e8eec2cb38c2d72e780e4
Shrrayashwinz <sshrrayash@gmail.com>
2025-12-12 19:11:50 -0500

    Got the code for hn_submissions.py in. Now to ensure its modified per the lab questions

--- lab17_ssrinivasan3-1.py
+++ lab17_ssrinivasan3-1.py
@@ -7,3 +7,52 @@ Purpose: Refactor the HN submission script to safely handle missing data fields 
 
 Date: December 11, 2025 
 """
+
+from operator import itemgetter
+import requests
+
+"""Get an API call and store the respone."""
+
+url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
+r = requests.get(url)
+print(f"Status code: {r.status_code}")
+
+"""Process information about each submission."""
+submission_ids = r.json()
+submission_dicts = []
+
+
+for submission_id in submission_ids[:30]:
+
+    """Make a separate API call for each submission."""
+
+
+    url = f"https://hacker-news.firebaseio.com/v0/item/{submission_id}.json"
+
+    r = requests.get(url)
+
+    response_dict = r.json()
+
+    """Build a dictionary for each submission."""
+
+    submission_dict = {
+        'title': response_dict.get('title', 'No Title'),
+
+        'link': f"http://news.ycombinator.com/item?id={submission_id}",
+
+        'comments': response_dict.get('descendants', 0)
+    }
+    submission_dicts.append(submission_dict)
+
+submission_dicts = sorted(submission_dicts, key=itemgetter('comments'), reverse=True)
+
+
+"""Print the top submissions."""
+for submission_dict in submission_dicts:
+    print(f"\nTitle: {submission_dict['title']}")
+    print(f"Discussion link: {submission_dict['link']}")
+    print(f"Comments: {submission_dict['comments']}")
+
+
+
+
